ec6bb597a37cecf1ce73cb17ff2c57c8debae58e
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2025-07-15 14:19:39 -0500

    fix/Values
    fix grid and pictures

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import css from "./Values.module.css";
 import { valuesData } from "./data";
@@ -5,19 +6,30 @@ import { valuesData } from "./data";
 const Values = () => {
   return (
     <div className={css.container}>
-      {valuesData.map(({ image: Icon, title, text }) => (
-        <div key={title} className={css.card}>
-          <div className={css.titleWrap}>
-            <Icon />
-            <h4 className={css.title}>{title}</h4>
+      {valuesData.map(({ image: Icon, title, text }, index) => (
+        <React.Fragment key={title}>
+          <div className={css.card}>
+            <div className={css.titleWrap}>
+              <Icon />
+              <h4 className={css.title}>{title}</h4>
+            </div>
+            <div className={css.textWrap}>
+              <p className={css.text}>{text}</p>
+            </div>
           </div>
-          <div className={css.textWrap}>
-            <p className={css.text}>{text}</p>
-          </div>
-        </div>
+
+          {index === 1 && (
+            <img src="/src/assets/images/man-worker.png" alt="man worker" />
+          )}
+          {index === 3 && (
+            <img
+              src="/src/assets/images/wind-farms-fields 1.png"
+              alt="wind filds"
+            />
+          )}
+        </React.Fragment>
       ))}
     </div>
   );
 };
-
 export default Values;
