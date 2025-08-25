9457b23dada96b76da3c1efacd7e5e1979e70e9f
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2025-08-25 12:55:58 -0500

    update for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react(), svgr()],
+  base: "/practice/",
 });
